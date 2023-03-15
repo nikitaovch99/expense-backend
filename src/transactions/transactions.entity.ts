6cb88ce5ea06e722f58521bbeca28cf1b9095689
Nikita <nikitaovch99@gmail.com>
2023-03-15 12:24:44 +0200

    feat: documented entitites

--- src/transactions/transactions.entity.ts
+++ src/transactions/transactions.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Category } from 'src/categories/categories.entity';
 import { User } from 'src/users/users.entity';
 import {
@@ -11,18 +12,25 @@ import {
 
 @Entity({ name: 'transactions' })
 export class Transaction {
+  @ApiProperty({ example: '1', description: 'unique identificator' })
   @PrimaryGeneratedColumn()
   id!: number;
-
+  @ApiProperty({ example: '-10', description: 'transaction label' })
   @Column()
   label!: string;
-
+  @ApiProperty({
+    example: '2023-03-10T09:00:32.011Z',
+    description: 'time of transaction',
+  })
   @CreateDateColumn()
   date!: Date;
-
+  @ApiProperty({
+    example: '2023-03-10T09:00:32.011Z',
+    description: 'time when the field was updated',
+  })
   @UpdateDateColumn()
   updatedAt: Date;
-
+  @ApiProperty({ example: 10, description: 'total amount after transaction' })
   @Column()
   amount!: number;
 
